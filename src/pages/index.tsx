16cac3fc7252ef64325cc75722f0571c12ec374e
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-01-13 14:12:43 -0300

    feat: implementa estilos globais da aplicação utilizando `@stitches/react`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 8,
+  border: 0,
+  padding: "4px 8px",
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
