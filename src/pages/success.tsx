830ce514d7d8c4c2bb0219563335634bac8b0e0b
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-01-25 13:12:11 -0300

    refactor: cria estrutura da página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link'
+import { SuccessContainer, ImageContainer } from '../styles/pages/success'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Ranielli Montagna</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
