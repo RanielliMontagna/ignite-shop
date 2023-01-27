683a56f173f2f98498e7c9c6280b453803a96292
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-01-27 16:37:33 -0300

    refactor: adiciona `head` das paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
+
 import Image from 'next/image'
+import Head from 'next/head'
 import Link from 'next/link'
 
 import { stripe } from '@/lib/stripe'
@@ -28,25 +30,34 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => (
-        <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-          <Product className="keen-slider__slide">
-            <Image
-              src={product.imageUrl}
-              alt="camiseta1"
-              width={520}
-              height={480}
-            />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => (
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+            prefetch={false}
+          >
+            <Product className="keen-slider__slide">
+              <Image
+                src={product.imageUrl}
+                alt="camiseta1"
+                width={520}
+                height={480}
+              />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        </Link>
-      ))}
-    </HomeContainer>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
+    </>
   )
 }
 
